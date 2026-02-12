293d3def17773e9d2206fc657301a1dea732905d
God-Incarnate <prashant11111999@gmail.com>
2026-02-12 19:51:50 +0530

    queue basic operations using Linked List.

--- src/data_structures/queues/BasicOperationsLL.java
+++ src/data_structures/queues/BasicOperationsLL.java
@@ -1,5 +1,34 @@
 package data_structures.queues;
 
+import data_structures.linked_list.ListNode;
+
 public class BasicOperationsLL {
+    static ListNode front, rear;
+
+    public static void main(String[] args) {
+        System.out.println("Adding Value to queue: ");
+        push(4);
+        push(6);
+        printQueue();
+    }
+
+    public static void push(int value) {
+        if (front==null){
+            front=new ListNode(value);
+            rear=front;
+            return;
+        }
+        rear.next=new ListNode(value);
+        rear=rear.next;
+    }
+
+    public static void printQueue() {
+        ListNode current=front;
+        while (current!=null){
+            System.out.print(current.val+" ");
+            current=current.next;
+        }
+    }
+
 
 }
