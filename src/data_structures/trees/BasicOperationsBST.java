52f3679276d4cbce8ef6c7c2ce9b3dddd1b61420
God-Incarnate <prashant11111999@gmail.com>
2026-02-15 23:42:14 +0530

    BST - insert

---
+++ src/data_structures/trees/BasicOperationsBST.java
@@ -0,0 +1,52 @@
+package data_structures.trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BasicOperationsBST {
+    static BTTreeNode bstTree;
+
+    static void main(String[] args) {
+        insert(8);
+        insert(4);
+        insert(10);
+        insert(3);
+        insert(6);
+        insert(9);
+        insert(12);
+        insert(5);
+        insert(7);
+        insert(11);
+//        System.out.println("Leve order traversal:::::");
+//        levelOrderPrint();
+//        System.out.println("\nInOrder traversal:::::");
+//        inOrderPrint(bstTree);
+    }
+
+    public static void insert(int x) {
+        if (bstTree==null) {
+            bstTree=new BTTreeNode(x);
+            return;
+        }
+        BTTreeNode current = bstTree;
+        BTTreeNode prev = bstTree;
+        while(current!=null){
+            if(current.val>x){
+                prev=current;
+                current=current.left;
+            } else if(current.val<x) {
+                prev=current;
+                current=current.right;
+            } else {
+                System.out.println("ERROR - Duplicate Entries Not Allowed!!!!.");
+                return;
+            }
+        }
+        current=new BTTreeNode(x);
+        if(prev.val>x){
+            prev.left=current;
+        } else {
+            prev.right=current;
+        }
+    }
+}
