3d71ea4db1118e6284b87758514d537939cbc701
God-Incarnate <prashant11111999@gmail.com>
2026-03-13 20:20:11 +0530

    Bubble Sort

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -6,6 +6,12 @@ public class BubbleSort {
 
     static int[] arr={2,4,6,3,2,1,7,5,9};
 
+    static void main(String[] args) {
+        bubbleSort();
+        System.out.println("Sorted Array Using BUBBLE SORT::::::");
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void bubbleSort() {
         for (int i = 0; i < arr.length - 1; i++) {
             boolean hasSwapped = false;
