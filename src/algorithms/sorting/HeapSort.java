7beaa8d7faf9da5be22055ad17003b7e82d490f7
God-Incarnate <prashant11111999@gmail.com>
2026-03-16 18:09:59 +0530

    Heap Sort

--- src/algorithms/sorting/HeapSort.java
+++ src/algorithms/sorting/HeapSort.java
@@ -25,5 +25,23 @@ public class HeapSort {
             heapify(i,0);
         }
     }
-    
+
+    public static void heapify(int n,int i){
+        int largest=i;
+        int left=(2*i)+1;
+        int right=(2*i)+2;
+        if(left<n && arr[left]>arr[largest]){
+            largest=left;
+        }
+        if (right<n && arr[right]>arr[largest]){
+            largest=right;
+        }
+        if(largest!=i){
+            int swap=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=swap;
+
+            heapify(n,largest);
+        }
+    }
 }
