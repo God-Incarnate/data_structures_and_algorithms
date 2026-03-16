b0a193ac09518c6d23eb58ab352c4057672df03a
God-Incarnate <prashant11111999@gmail.com>
2026-03-16 18:06:56 +0530

    Counting Sort

--- src/algorithms/sorting/CountingSort.java
+++ src/algorithms/sorting/CountingSort.java
@@ -11,4 +11,31 @@ public class CountingSort {
         System.out.println(Arrays.toString(arr));
     }
 
+    public static void countingSort(){
+        int n=arr.length-1;
+        int max=0;
+        for (int i=0;i<=n;i++) {
+            if(arr[i]>max) {
+                max=arr[i];
+            }
+        }
+
+        int[] count=new int[max+1];
+        for(int num:arr){
+            count[num]++;
+        }
+
+        for (int i=1;i<count.length;i++){
+            count[i]=count[i]+count[i-1];
+        }
+        int[] output=new int[n+1];
+        for(int i=n;i>=0;i--){
+            output[count[arr[i]]-1]=arr[i];
+            count[arr[i]]--;
+        }
+
+        for (int i=0;i<n;i++){
+            arr[i]=output[i];
+        }
+    }
 }
