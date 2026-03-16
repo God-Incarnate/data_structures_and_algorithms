de6995344e370324a30d5ad7138c519105c89300
God-Incarnate <prashant11111999@gmail.com>
2026-03-16 18:09:25 +0530

    Heap Sort

--- src/algorithms/sorting/HeapSort.java
+++ src/algorithms/sorting/HeapSort.java
@@ -10,4 +10,20 @@ public class HeapSort {
         System.out.println(Arrays.toString(arr));
     }
 
+    public static void heapSort(){
+        int n=arr.length;
+        for (int i=n/2-1;i>=0;i--){
+            heapify(n,i);
+        }
+
+        for (int i=n-1;i>0;i--) {
+
+            int swapRoot=arr[i];
+            arr[i]=arr[0];
+            arr[0]=swapRoot;
+
+            heapify(i,0);
+        }
+    }
+    
 }
