50d427093827ba029d0c28ed2777fa341a8da77f
God-Incarnate <prashant11111999@gmail.com>
2026-03-16 18:08:57 +0530

    Heap Sort

---
+++ src/algorithms/sorting/HeapSort.java
@@ -0,0 +1,13 @@
+package algorithms.sorting;
+
+import java.util.Arrays;
+
+public class HeapSort {
+    static int[] arr={2,4,6,3,2,1,7,5,9};
+
+    static void main(String[] args){
+        heapSort();
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
