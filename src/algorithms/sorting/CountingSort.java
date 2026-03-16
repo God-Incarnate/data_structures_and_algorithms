a32e0220edcd3747e52f37109966d797e4b6baf7
God-Incarnate <prashant11111999@gmail.com>
2026-03-16 18:06:14 +0530

    Counting Sort

---
+++ src/algorithms/sorting/CountingSort.java
@@ -0,0 +1,14 @@
+package algorithms.sorting;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    static int[] arr={2,4,6,3,2,1,7,5,9};
+
+    static void main(String[] args) {
+
+        countingSort();
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
