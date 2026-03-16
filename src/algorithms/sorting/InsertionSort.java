4df081ca3cbd0c8848271ee0c3b2306a828c9e8b
God-Incarnate <prashant11111999@gmail.com>
2026-03-16 18:10:53 +0530

    Insertion Sort

---
+++ src/algorithms/sorting/InsertionSort.java
@@ -0,0 +1,14 @@
+package algorithms.sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    static int[] arr={2,4,6,3,2,1,7,5,9};
+
+    static void main(String[] args){
+        insertionSort();
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
