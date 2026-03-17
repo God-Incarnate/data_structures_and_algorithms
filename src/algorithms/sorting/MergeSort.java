71157b67ea8767ca5f8372699062c657a8fe07c2
God-Incarnate <prashant11111999@gmail.com>
2026-03-17 22:09:50 +0530

    Merge Sort

--- src/algorithms/sorting/MergeSort.java
+++ src/algorithms/sorting/MergeSort.java
@@ -5,5 +5,10 @@ import java.util.Arrays;
 public class MergeSort {
     static int[] arr={2,4,6,3,2,1,7,5,9};
 
+    static void main(String[] args) {
+        mergeSort(0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    
 }
