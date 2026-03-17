a1489ca20fbbf63f0883811d98c9aaf1eb24e13d
God-Incarnate <prashant11111999@gmail.com>
2026-03-17 22:10:34 +0530

    Merge Sort

--- src/algorithms/sorting/MergeSort.java
+++ src/algorithms/sorting/MergeSort.java
@@ -19,5 +19,42 @@ public class MergeSort {
         }
     }
 
-    
+    public static void merge(int left, int mid, int right) {
+        int n1=mid-left+1;
+        int n2=right-mid;
+        int[] ar1=new int[n1];
+        int[] ar2=new int[n2];
+
+        for (int i=0;i<n1;i++) {
+            ar1[i]=arr[left+i];
+        }
+
+        for (int j=0;j<n2;j++) {
+            ar2[j]=arr[mid+j+1];
+        }
+
+        int i=0,j=0;
+        int k=left;
+        while (i<n1 && j<n2){
+            if (ar1[i]<=ar2[j]) {
+                arr[k]=ar1[i];
+                i++;
+            }else {
+                arr[k]=ar2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<n1){
+            arr[k]=ar1[i];
+            i++;
+            k++;
+        }
+        while (j<n2){
+            arr[k]=ar2[j];
+            j++;
+            k++;
+        }
+
+    }
 }
