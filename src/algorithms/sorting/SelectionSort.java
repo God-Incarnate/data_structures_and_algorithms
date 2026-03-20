61ac12ea418d597c4b28c72a78f9b120c7bef67d
God-Incarnate <prashant11111999@gmail.com>
2026-03-20 15:06:57 +0530

    Selection Sort

---
+++ src/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,15 @@
+package algorithms.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    static int[] arr={2,4,6,3,2,1,7,5,9};
+
+    static void main(String[] args){
+
+        selectionSort();
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
