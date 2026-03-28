2866801d8d9232f04fa3fba2b7e2023858bd99f7
God-Incarnate <prashant11111999@gmail.com>
2026-03-29 00:21:00 +0530

    Streams -> Intermediate Operations in Stream-> part of Functional Interface--Implementation

--- src/algorithms/streams/IntermediateOperationsDemo.java
+++ src/algorithms/streams/IntermediateOperationsDemo.java
@@ -7,6 +7,18 @@ public class IntermediateOperationsDemo {
 
     public static void main(String[] args) {
 
+        List<Integer> list = Arrays.asList(5, 2, 8, 1, 3, 2, 4);
 
+        List<Integer> result = list.stream()
+                .filter(x -> x > 2)          // filter
+                .map(x -> x * 2)             // map
+                .distinct()                  // remove duplicates
+                .sorted()                   // ascending sort
+                .peek(x -> System.out.println("Processing: " + x)) // debug
+                .limit(4)                   // limit
+                .skip(1)                    // skip
+                .collect(Collectors.toList()); // terminal (trigger)
+
+        System.out.println("Final Result: " + result);
     }
 }
