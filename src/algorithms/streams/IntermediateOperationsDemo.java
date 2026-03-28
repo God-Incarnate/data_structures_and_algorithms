5bb4d78f2b76b31015cc52562ce600f3577548d4
God-Incarnate <prashant11111999@gmail.com>
2026-03-29 00:20:31 +0530

    Streams -> Intermediate Operations in Stream-> part of Functional Interface

---
+++ src/algorithms/streams/IntermediateOperationsDemo.java
@@ -0,0 +1,12 @@
+package algorithms.streams;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class IntermediateOperationsDemo {
+
+    public static void main(String[] args) {
+
+
+    }
+}
