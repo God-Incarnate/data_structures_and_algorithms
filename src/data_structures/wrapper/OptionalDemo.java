b6938a8634d475a423426bd8320d99dff29b195d
God-Incarnate <prashant11111999@gmail.com>
2026-03-31 21:47:22 +0530

    Optional class

---
+++ src/data_structures/wrapper/OptionalDemo.java
@@ -0,0 +1,22 @@
+package data_structures.wrapper;
+
+import java.util.*;
+
+public class OptionalDemo {
+
+    static class User {
+        String name;
+
+        User(String name) {
+            this.name = name;
+        }
+
+        String getName() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
