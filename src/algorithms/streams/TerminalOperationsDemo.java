905045c9a35e58d17fb931260fe9ed6db579ed18
God-Incarnate <prashant11111999@gmail.com>
2026-04-05 18:58:35 +0530

    Streams -> Terminal Operations

---
+++ src/algorithms/streams/TerminalOperationsDemo.java
@@ -0,0 +1,12 @@
+package algorithms.streams;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class TerminalOperationsDemo {
+
+    public static void main(String[] args) {
+
+
+    }
+}
