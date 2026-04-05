ff294511d39751b14d008494a3f3d7174b0d415d
God-Incarnate <prashant11111999@gmail.com>
2026-04-05 18:58:57 +0530

    Streams -> Terminal Operations

--- src/algorithms/streams/TerminalOperationsDemo.java
+++ src/algorithms/streams/TerminalOperationsDemo.java
@@ -7,6 +7,44 @@ public class TerminalOperationsDemo {
 
     public static void main(String[] args) {
 
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
 
+        // forEach
+        list.stream().forEach(x -> System.out.println("forEach: " + x));
+
+        // collect
+        List<Integer> collected =
+                list.stream()
+                        .filter(x -> x > 2)
+                        .collect(Collectors.toList());
+        System.out.println("collect: " + collected);
+
+        // count
+        long count = list.stream().count();
+        System.out.println("count: " + count);
+
+        // reduce
+        int sum = list.stream().reduce(0, Integer::sum);
+        System.out.println("reduce (sum): " + sum);
+
+        // findFirst
+        Optional<Integer> first =
+                list.stream().findFirst();
+        System.out.println("findFirst: " + first.get());
+
+        // anyMatch
+        boolean anyMatch =
+                list.stream().anyMatch(x -> x > 3);
+        System.out.println("anyMatch (>3): " + anyMatch);
+
+        // allMatch
+        boolean allMatch =
+                list.stream().allMatch(x -> x > 0);
+        System.out.println("allMatch (>0): " + allMatch);
+
+        // noneMatch
+        boolean noneMatch =
+                list.stream().noneMatch(x -> x < 0);
+        System.out.println("noneMatch (<0): " + noneMatch);
     }
 }
